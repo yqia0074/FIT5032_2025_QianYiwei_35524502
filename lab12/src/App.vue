6e3bce1500f3fdefd0e2e129440a034dbae01d82
yqia0074 <yqia0074@student.monash.edu>
2025-08-14 15:41:24 +0800

    lab12

---
+++ lab12/src/App.vue
@@ -0,0 +1,14 @@
+
+<script setup>
+import JSONLab from './components/JSON.vue'
+import Form from './components/Form.vue'
+</script>
+
+<template>
+  <Form />
+  <!-- <JSONLab /> -->
+</template>
+
+<style>
+/* You can add global styles here */
+</style>
